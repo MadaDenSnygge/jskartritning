9a31ca4ab3cbd3a5a34a02f2b57c5cf00f1b4fee
Mada <adam.filip.samuelsson@gmail.com>
2025-10-30 17:35:13 +0100

    starting

---
+++ index.js
@@ -0,0 +1,9 @@
+    let titel = document.getElementById("titel")
+function enter(x) {
+    console.log("snopp")
+    document.getElementById(x).style.backgroundcolor="green"
+}
+function leave(x){
+    console.log("snopp")
+    document.getElementById(x).style.backgroundcolor="black"
+}
