191b9e2b7e2ef030528fdbb954b679ca727e9b01
Mada <adam.filip.samuelsson@gmail.com>
2025-11-19 15:16:57 +0100

    blandar

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
     let titel = document.getElementById("titel")
 function enter(x) {
-    console.log("snopp")
-    document.getElementById(x).style.backgroundcolor="green"
+    document.getElementById(x).style.borderBottomColor = "red"
 }
 function leave(x){
-    console.log("snopp")
-    document.getElementById(x).style.backgroundcolor="black"
+    document.getElementById(x).style.borderBottomColor = "lightslategray"
 }
+
+function switchPage(page){
+    window.location.href = page;
+}
+
+function enter2(x){
+    document.getElementById(x).style.scale = "1.2"
+}
+function leave2(x){
+document.getElementById(x).style.scale = "1"}
