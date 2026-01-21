2454fca3cc3289588fbed3fd88c04b72f3bde5cc
Mada <adam.filip.samuelsson@gmail.com>
2026-01-21 07:43:47 +0100

    telefon anpassning nästan klar

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ function enter2(x){
 }
 function leave2(x){
 document.getElementById(x).style.scale = "1"}
+
+value = 1
+
+function show(){
+    if(value == 1){
+        document.getElementById("linkHolderButton").style.visibility = "hidden";
+        document.getElementById("dropdownMenu").style.visibility = "visible";
+        value = 2
+    }
+    else if(value == 2){
+        document.getElementById("linkHolderButton").style.visibility = "visible"
+        document.getElementById("dropdownMenu").style.visibility = "hidden";
+        value = 1
+    }
+}
