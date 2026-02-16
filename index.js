02ff1f87c556bd68e7e686cb0c109203ee8e3541
MadaDenSnygge <adam.filip.samuelsson@gmail.com>
2026-02-16 16:04:34 +0100

    små ändringar

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
 function enter(x) {
-    // document.getElementById(x).style.borderBottomColor = "red"
     document.getElementById(x).style.cursor = "pointer"
 }
-function leave(x){
-    // document.getElementById(x).style.borderBottomColor = "lightslategray"
-}
+
+
 
 function switchPage(page){
     window.location.href = page;
 }
 
-function enter2(x){
-    document.getElementById(x).style.scale = "1.2"
-}
-function leave2(x){
-document.getElementById(x).style.scale = "1"}
 
-// value = 0
-
-// function show(add){
-//     value += add
-//     console.log(value)
-//     if(value > 1){
-//         document.getElementById("linkHolderButton").style.visibility = "hidden";
-//         document.getElementById("dropdownMenu").style.visibility = "visible";
-//     }
-//     else{
-//         document.getElementById("linkHolderButton").style.visibility = "visible"
-//         document.getElementById("dropdownMenu").style.visibility = "hidden";
-//     }
-//     if(value < 1){
-//         value = 0;
-//     }
-// }
 
 window.onclick = function (event) {
     if(event.target.matches("#linkImg")){
@@ -43,4 +19,11 @@ window.onclick = function (event) {
         document.getElementById("linkHolderButton").style.visibility = "visible"
         document.getElementById("dropdownMenu").style.visibility = "hidden";
     }
+}
+
+window.onload = function(){
+    console.log(document.getElementById("jan").style.height)
+    console.log(document.getElementById("jan").style.width)
+
+    document.getElementById("jan").style.height = document.getElementById("jan").style.width
 }
